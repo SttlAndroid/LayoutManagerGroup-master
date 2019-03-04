6c2db4478252ab72bfd569fa928d00473005e81c
unknown <smit_shah@STNW7-SMITMOBIL.silvertouch.com>
2019-03-04 17:35:30 +0530

    Initial commit

---
+++ LayoutManagerGroup/src/main/java/com/dingmouren/layoutmanagergroup/echelon/LayoutManagerEchelon.java
@@ -0,0 +1,29 @@
+package com.dingmouren.layoutmanagergroup.echelon;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Smit Shah on Mar 01 2019, 11:23 AM
+ */
+public class LayoutManagerEchelon extends RecyclerView.LayoutManager {
+
+    private int mItemViewHeight, mItemViewWidth, mItemCount, mScrollOffset = Integer.MAX_VALUE;
+    private float mScale = 0.9f;
+    private Context mContext;
+
+    public LayoutManagerEchelon(Context context) {
+        this.mContext = context;
+        mItemViewWidth = (int) (getHorizontalSpace() * 0.87f);
+        mItemViewHeight = (int) (mItemViewWidth * 1.46f);
+    }
+
+    @Override public RecyclerView.LayoutParams generateDefaultLayoutParams() {
+        return new RecyclerView.LayoutParams(RecyclerView.LayoutParams.WRAP_CONTENT,
+                RecyclerView.LayoutParams.WRAP_CONTENT);
+    }
+
+    private int getHorizontalSpace() {
+        return getWidth() - getPaddingLeft() - getPaddingRight();
+    }
+}
